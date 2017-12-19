0b308a284852deaeec413bf9b284746ffd9c9d5f
穆 一冰 <bokuyihi@bokumatombp.home>
2017-12-18 21:18:07 -0800

    register

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const mongoose = require('mongoose');
 const config = require('./config/database.js');
+const bodyParser = require('body-parser');
 const path = require('path');
+const authentication = require('./routes/authentication')(router);
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, (err) => {
     if (err) {
         console.log('Cannot connect to database', err);
     } else {
-        console.log('Connect to database' + config.db);
+        console.log('Connect to database: ' + config.db);
     }
 });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/client/dist'));
+app.use('/authentication', authentication);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/src/index.html'));
---
+++ models/user.js
@@ -0,0 +1,129 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
+
+let emailLengthChecker = (email) => {
+    if (!email) {
+        return false;
+    } else {
+        if (email.length < 5 || email.length > 30) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+};
+
+let validEmailChecker = (email) => {
+    if (!email) {
+        return false;
+    } else {
+        const regExp = new RegExp(/^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/);
+        return regExp.test(email);
+    }
+};
+
+let usernameLengthChecker = (username) => {
+    if (!username) {
+        return false;
+    } else {
+        if (username.length < 3 || username.length > 15) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+};
+
+let validUsernameChecker = (username) => {
+    if (!username) {
+        return false;
+    } else {
+        const regExp = new RegExp(/^[a-zA-Z0-9_-]{3,15}$/);
+        return regExp.test(username);
+    }
+};
+
+let passwordLengthChecker = (password) => {
+    if (!password) {
+        return false;
+    } else {
+        if (password.length < 6 || password.length > 16) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+};
+
+let validPasswordChecker = (password) => {
+    if (!password) {
+        return false;
+    } else {
+        const regExp = new RegExp(/^[a-zA-Z0-9_-]{6,16}$/);
+        return regExp.test(password);
+    }
+};
+
+const emailValidators = [
+    {   validator: emailLengthChecker,
+        message: 'E-mail must be at least 5 characters but no more than 30'
+    },
+    {   validator: validEmailChecker,
+        message: 'Must be a valid email'
+    }
+];
+
+const usernameValidators = [
+    {   validator: usernameLengthChecker,
+        message: 'Username must be at least 3 characters but no more than 15'
+    },
+    {   validator: validUsernameChecker,
+        message: 'Username cannot contain illegal characters'
+    }
+];
+
+const passwordValidators = [
+    {   validator: passwordLengthChecker,
+        message: 'Password must be at least 6 characters but no more than 16'
+    },
+    {   validator: validPasswordChecker,
+        message: 'Password cannot contain illegal characters'
+    }
+];
+
+
+ const userSchema = new Schema({
+    email: {type: String,
+            unique: true,
+            required: true,
+            lowercase: true,
+            validate: emailValidators
+    },
+     username: {type: String,
+                unique: true,
+                required: true,
+                lowercase: true,
+                validate: usernameValidators
+     },
+     password: {type: String,
+                required: true,
+                validate: passwordValidators
+    }
+ });
+
+ userSchema.pre('save', function (next) {
+     if (!this.isModified('password')) {
+         return next();
+     }
+
+     bcrypt.hash(this.password, null, null, (err, hash) => {
+         if (err) {
+             return next(err);
+         }
+         this.password = hash;
+         next();
+     });
+ });
+ module.exports = mongoose.model('User', userSchema);
