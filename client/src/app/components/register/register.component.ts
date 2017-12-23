3924507d746cb1816f8fc5e235c3e0b4a4479db6
穆 一冰 <bokuyihi@bokumatombp.home>
2017-12-23 00:00:07 -0800

    unfinished Register form

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.createForm();
+  }
+
+  createForm(){
+    this.form = this.formBuilder.group({
+      username: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(15)
+      ])],
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(30)
+      ])],
+      password: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(35)
+      ])],
+      confirm: ['', Validators.required]
+    })
+  }
+
+  onRegisterSubmit(){
+    console.log(this.form);
+  }
 
   ngOnInit() {
   }
